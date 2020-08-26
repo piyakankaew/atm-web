123b8c22d40baab8c41b413b644edf439ddf480b
piyakankaew <piyakan.kae@ku.th>
2020-08-26 14:18:09 +0700

    commit

--- src/main/java/th/go/rd/atm/controller/HomeController.java
+++ src/main/java/th/go/rd/atm/controller/HomeController.java
@@ -1,13 +1,15 @@
 package th.go.rd.atm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String getHomepage(){
+    public String getHomepage(Model model){
+        model.addAttribute("greeting","Sood Pang!!!!");
         return "home";  //home.html
 
     }
